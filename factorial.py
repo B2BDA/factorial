a8ff2d01ef4cb1850424a6670d520740d6f3e9c1
B2BDA <48247827+B2BDA@users.noreply.github.com>
2020-03-07 14:53:08 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -4,3 +4,9 @@ def factorial(a):
     else:
         return factorial(a-1)*a
 print(factorial(4))
+
+def fact(n):
+    l=list(range(1,n+1))
+    return reduce(lambda x,y: x*y, l)
+
+[fact(i) for i in l1]
